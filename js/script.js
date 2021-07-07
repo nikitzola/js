9cc962738f30e63733d61ce8793344302e53cf49
nikuta <nikitazolotyh2008@gmail.co>
2021-07-07 15:11:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+let incr = 10,
+decr = 10;
+
+/**++incr;
+decr--;**/
+console.log (incr++);
+console.log (decr--);
+
+console.log('2' + "-object");
+console.log(4 + + "5");
+
+console.log(5%2);
+
+const isCheked = true,
+isClase= true;
+console.log(isCheked && isClase);
